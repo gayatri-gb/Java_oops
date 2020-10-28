45af2b2d0f415f24e7262aad1f4f40e224a0e06e
gayatri-gb <gayatribhatnagar8@gmail.com>
2020-10-27 23:23:18 -0500

    commit

---
+++ JavaCoreConcepts/src/multiThreading/WaitAndNotify.java
@@ -0,0 +1,62 @@
+package multiThreading;
+
+public class WaitAndNotify {
+	static public int balance=0;
+	
+	
+	public void withdraw(int amount) {
+		synchronized(this){
+		if(balance<=0) {
+			try {
+				System.out.println("Waiting for update");
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}// wait method is stop for sometime until notify method is executed,
+			
+			balance= balance- amount;
+			System.out.println("withdrawal succesfull");
+		}
+		}
+		
+	}
+	
+	public void deposit(int amount) {
+		System.out.println("Depositing amount");
+		balance= balance+amount;
+		synchronized(this){
+		notifyAll();
+	}
+	}
+
+	public static void main(String[] args) {
+		WaitAndNotify object= new WaitAndNotify();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				object.withdraw(1000);
+				
+			}
+			
+		} ).start();
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			object.deposit(2000);
+				
+			}
+			
+		} ).start();
+	}
+
+}
